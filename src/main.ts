9fc445e0733ce6ccd1715f64227433e0aef94bb9
Sviatolsav <91410097+ra6ty@users.noreply.github.com>
2025-04-16 14:38:47 +0300

    *add fix

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,8 @@ async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
     transport: Transport.TCP,
     options: {
-      port: Number(process.env.PORT),
+      host: '0.0.0.0',
+      port: Number(process.env.PORT) || 3002,
     },
   });
 
