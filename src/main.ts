0e0f9ba0ea71d3c9a890d6a7194565c8daecc54a
Sviatolsav <91410097+ra6ty@users.noreply.github.com>
2025-04-16 10:48:21 +0300

    *add fix

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,11 @@ async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
     transport: Transport.TCP,
     options: {
-      port: Number(process.env.PORT) || 3002,
-      host: '0.0.0.0'
+      port: Number(process.env.PORT) || 3002, // 👈 Оце обов'язково
     },
   });
 
   await app.listen();
-  console.log('File Service is running on TCP port 3002');
+  console.log('File Service is running on TCP port', process.env.PORT);
 }
 bootstrap();
